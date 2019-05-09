e6d7610a97ff39b446d521b570401c54a6181207
James Declerus <james.declerus@esih.edu>
2019-05-09 15:59:39 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,22 +1,22 @@
 
 class Course
- 
+
   attr_accessor :title, :schedule, :description
- 
+
   @@all = []
- 
+
   def initialize
     @@all << self
   end
- 
+
   def self.all
     @@all
   end
- 
+
   def self.reset_all
     @@all.clear
   end
- 
+
 end
 Run the Course specs again and we should be passing all the tests. We're ready to write our Scraper!
 
